ccda5a35a42eeef24c3dca5ae65554db1c29a157
SugarWater13 <mr.ajselig@gmail.com>
2021-07-15 11:11:01 -0600

    node?

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+
+
 window.drawRect = function drawRect(context, margin, color, x, y, width, height) {
     context.fillStyle = color
     context.fillRect(x + margin, y + margin, width, height);
@@ -13,7 +15,7 @@ function Plat(context, margin, color, x, y, width, height) {
     this.height = height;
 }
 
-export const level={}
+export const level = {}
 
 window.drawMap = function drawMap(context, margin, width, height) {
 
@@ -23,20 +25,30 @@ window.drawMap = function drawMap(context, margin, width, height) {
     //platform width defined by characterWith*n as to adjust with screen size
     var platWidth = width / 20
 
-    //draw floor
-    //drawRect(context, margin, "#00670c", 0, height/2, width, height);
-
-    //delete me
-    //drawRect(context, margin,'#000000', width*19/20-platWidth, 250, platWidth*2, 10)
-
     //define platforms
-    const plat0 = new Plat(context, margin, "#00670c", 0, height * 19 / 20, width, height)
-    const plat1 = new Plat(context, margin, '#000000', width * 18 / 20 - platWidth, height * 13 / 20, platWidth * 2, 10);
-    const plat2 = new Plat(context, margin, '#FFFFFF', width * 15 / 20 - platWidth, height * 16 / 20, platWidth * 3, 10);
+    var levelMap = []
+    
+    fs.readFile('level1.json', (err, data) => {
+        if (err) return
+        
+        level1 = JSON.parse(data)
+    })
+    
+    const mapData = JSON.parse(level1)
+
+    for (i = 0; i < mapData.length; i++) {
+        newPlat = new Plat(mapData(i).context, mapData(i).margin, mapData(i).color, mapData(i).x, mapData(i).y, mapData(i).width, mapData(i).height)
+        levelMap.push(newPlat)
+    }
+
+
+    // const plat0 = new Plat(context, margin, '#00670c', 0, height * 19 / 20, width, height / 20)
+    // const plat1 = new Plat(context, margin, '#FFFFFF', width * 15 / 20 - platWidth, height * 16 / 20, platWidth * 3, 10);
+    // const plat2 = new Plat(context, margin, '#000000', width * 18 / 20 - platWidth, height * 13 / 20, platWidth * 2, 10);
 
     // //define map
-    var levelMap = [plat0, plat1, plat2]
-    level.levelMap=levelMap
+    // var levelMap = [plat0, plat1, plat2]
+    level.levelMap = levelMap
 
     // //draw map
     for (var i = 0; i < levelMap.length; i++) {
